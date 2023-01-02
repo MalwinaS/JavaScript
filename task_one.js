649f7d008adbded00ad979541fb2de599291b600
MalwinaS <malwinastepien93@gmail.com>
2023-01-02 18:37:58 +0100

    tasks nr 1 and 2

---
+++ task_one.js
@@ -0,0 +1,278 @@
+// const text1 = 'powiększ mnie!'
+// const text1a = text1.toUpperCase();
+// console.log(text1a);
+
+// const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+// const text2a = text2.toLowerCase();
+// console.log(text2a);
+
+// const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+// const text3a = text3.slice(6);
+// console.log(text3a);
+
+// const text4 = 'sprawdź, czy zawieram słowo "czy"'
+// const text4a = text4.includes('czy');
+// console.log(text4a);
+
+// const text5 = 'wyLoguj w const text1 = 'powiększ mnie!'
+// const text1a = text1.toUpperCase();
+// console.log(text1a);
+
+// const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+// const text2a = text2.toLowerCase();
+// console.log(text2a);
+
+// const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+// const text3a = text3.slice(6);
+// console.log(text3a);
+
+// const text4 = 'sprawdź, czy zawieram słowo "czy"'
+// const text4a = text4.includes('czy');
+// console.log(text4a);
+
+// const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+// // const index = 'L';
+// const text5a = text5.charAt(2);
+// console.log(text5a);
+
+// const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+// const text6a = text6.replaceAll('pies', 'kot');
+// console.log(text6a);
+
+// const text7 = 'podziel, ten, string, od, przecinków'
+// const text7a = text7.split(',');
+// console.log(text7a);
+
+
+// const numbers = [0, 0, 1, 1, 2, 2, 2];
+// const colors = ['red', 'green', 'blue', true, 123];
+// const cars = [123, true, 'audi', 'bmw', 'mercedes', 'ferrari'];
+
+// let numbers2 = numbers.slice(0, 2);
+// console.log(numbers2);
+
+// let numbers3 = numbers.slice(-3);
+// console.log(numbers3);
+
+// let colors2 =colors.splice(-2);
+// console.log(colors2);
+
+// let randomStaff = colors;
+// console.log(randomStaff);
+
+// let newCars = cars.splice(2, 4, 'test');
+// console.log(newCars);
+// console.log(cars);
+
+
+// const letters = ['c', 'd'];
+// letters.push('e', 'f');
+// letters.unshift('a','b');
+// console.log(letters);
+// console.log(letters.includes('c'));
+
+// const x = [1, 2, 3];
+// const y = ['apple', 'pizza', 'pasta'];
+
+// const newXy = x.concat(y);
+// console.log(...newXy);
+
+// const tabs = [1, 5, 13, 26, 48];
+// const newTab = tabs.map(tab => console.log(tab * 5));
+// console.log(newTab);
+
+
+// const tabs2 = [1, 5, 13, 26, 48];
+
+// // if (tab % 2 === 0) {
+// //     tabs2.map(tab);
+// //     console.log (Liczba parzysta: {tab});
+// // } else {
+// //     console.log(Liczba nieparzysta: {tab});
+// // }
+
+// for(const tab of tabs2) {
+//     if (tab % 2 === 0) {
+//     console.log (`Liczba parzysta: ${tab}`);
+// } else {
+//     console.log (`Liczba nieparzysta: ${tab}`);
+// }
+// }
+
+// const ari = ['black'];
+// ari.unshift('white');
+// ari.push('blue');
+// // console.log(ari);
+
+// for (const i=0; i < ari.length; i++) {
+//     const y = ari[1];
+//     console.log(`Moj ulubiony kolor to: ${y[0].toUpperCase() + y.slice(1)}`)konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+// // const index = 'L';
+// const text5a = text5.charAt(2);
+// console.log(text5a);
+
+// const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+// const text6a = text6.replaceAll('pies', 'kot');
+// console.log(text6a);
+
+// const text7 = 'podziel, ten, string, od, przecinków'
+// const text7a = text7.split(',');
+// console.log(text7a);
+
+
+// const numbers = [0, 0, 1, 1, 2, 2, 2];
+// const colors = ['red', 'green', 'blue', true, 123];
+// const cars = [123, true, 'audi', 'bmw', 'mercedes', 'ferrari'];
+
+// let numbers2 = numbers.slice(0, 2);
+// console.log(numbers2);
+
+// let numbers3 = numbers.slice(-3);
+// console.log(numbers3);
+
+// let colors2 =colors.splice(-2);
+// console.log(colors2);
+
+// let randomStaff = colors;
+// console.log(randomStaff);
+
+// let newCars = cars.splice(2, 4, 'test');
+// console.log(newCars);
+// console.log(cars);
+
+
+// const letters = ['c', 'd'];
+// letters.push('e', 'f');
+// letters.unshift('a','b');
+// console.log(letters);
+// console.log(letters.includes('c'));
+
+// const x = [1, 2, 3];
+// const y = ['apple', 'pizza', 'pasta'];
+
+// const newXy = x.concat(y);
+// console.log(...newXy);
+
+// const tabs = [1, 5, 13, 26, 48];
+// const newTab = tabs.map(tab => console.log(tab * 5));
+// console.log(newTab);
+
+
+// const tabs2 = [1, 5, 13, 26, 48];
+
+// // if (tab % 2 === 0) {
+// //     tabs2.map(tab);
+// //     console.log (Liczba parzysta: {tab});
+// // } else {
+// //     console.log(Liczba nieparzysta: {tab});
+// // }
+
+// for(const tab of tabs2) {
+//     if (tab % 2 === 0) {
+//     console.log (`Liczba parzysta: ${tab}`);
+// } else {
+//     console.log (`Liczba nieparzysta: ${tab}`);
+// }
+// }
+
+// const ari = ['black'];
+// ari.unshift('white');
+// ari.push('blue');
+// // console.log(ari);
+
+// for (const i=0; i < ari.length; i++) {
+//     const y = ari[1];
+//     console.log(`Moj ulubiony kolor to: ${y[0].toUpperCase() + y.slice(1)}`)
+// }
+
+// const str = 'Mercedes, aaa, BMW, Nissan, Dodge';
+// const strTab = str.split(', ');
+// console.log(strTab);
+
+// if (strTab.includes('Audi')) {
+//      strTab.push('Skoda');
+// } else {
+//     strTab.pop();
+// }
+
+// console.log(strTab);
+
+
+// let score;
+
+// function add (x, y) {
+//     score = x + y;
+//     if (score % 2 === 0) {
+//         parzyste();
+//     } else {
+//         nieparzyste();
+//     }
+// }
+
+// const parzyste = () => console.log(`Liczba ${score} jest parzysta`);
+// const nieparzyste = () => console.log(`Liczba ${score} jest nieparzysta`);
+
+// add(10,10);
+// function parzyste () {
+//     console.log(`Liczba jest parzysta`);
+// }
+
+
+// let celsius;
+// let temp;
+
+// function farenheit (x) {
+//     celsius = x;
+//     temp = celsius * 1.8 + 32;
+//     console.log(`${celsius} C = ${temp} F`);
+// }
+
+// farenheit(12);
+
+// let num = 10;
+// let numbers = [];
+
+
+// for (let i = 0; i < num; i++) {
+//     numbers.push(i);
+// }
+
+// const one = x => {
+//     if (x % 3 === 0 && x !== 0) {
+//         console.log(`${x} jest podzielne przez 3`);
+//     } else {
+//         console.log(`${x} nie jest podzielne przez 3 lub x = 0`);
+//     }
+// }
+
+
+// numbers.forEach(one);
+
+// const heading = document.querySelector('.test');
+// console.log(heading);
+
+// const parTab = document.querySelectorAll('p');
+// console.log(parTab);
+
+// const divcass = document.querySelector('.test');
+// console.log(divcass);
+// const par = document.createElement('p');
+// divcass.appendChild(par);
+
+// const divcasspar = divcass.querySelector('#test');
+// console.log(divcasspar);
+
+const lista = document.createElement('ul');
+const number = 10;
+document.body.append(lista);
+
+for (let i = 1; i <= number; i++) {
+const liItem = document.createElement('li');
+liItem.textContent = i;
+lista.appendChild(liItem);
+}
+
+const lastItem = lista.lastChild;
+lastItem.textContent = 'Jestem ostatnim elementem';
+lastItem.style.color = 'red';
+lastItem.add('test');
---
+++ zad2/drugi.js
@@ -0,0 +1,25 @@
+const btn1 = document.querySelector('btn1');
+const btn2 = document.querySelector('btn2');
+const square = document.querySelector('square');
+const p1 = document.querySelector('p1');
+const p2 = document.querySelector('p2');
+
+const fun1 () => {
+    console.log(czesc);
+}
+
+const fun2 () => {
+    square.style.background = 'red';
+}
+
+const fun3 () => {
+    square.style.background = 'blue';
+}
+
+
+const fun4 () => {
+    p1.classList.toggle('show');
+    p2.classList.toggle('show');
+}
+
+
